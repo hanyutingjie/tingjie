aff97e102526d30d843f0325afff892a2f32b5f9
Administrator <Administrator@LUTM4AVP4YUGPYX>
2019-06-04 16:27:45 +0800

    author:tingting time:2019-6-4

---
+++ 222/src/ting/ad.java
@@ -0,0 +1,14 @@
+package ting;
+
+public class ad {
+
+	public ad() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("123");
+	}
+
+}
